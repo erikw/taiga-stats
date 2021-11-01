f45ecd72776b5599ab0b2c7d19f9c9667ef46536
Erik Westrup <erik.westrup@gmail.com>
2021-11-01 18:03:54 +0100

    Flake8 helpers.py

--- taiga_stats/helpers.py
+++ taiga_stats/helpers.py
@@ -1,27 +1,12 @@
-from pprint import pprint
-import datetime as dt
-import os
-import re
-import sys
-import time
-import configparser
+import taiga_stats
 
-import taiga
-import numpy
+import datetime as dt
+import sys
 
 import matplotlib
 matplotlib.use('TkAgg')  # Reference: https://stackoverflow.com/a/48374671/265508
-import matplotlib.pyplot as plt
-import matplotlib.lines as mplotlines
-import matplotlib.dates as mplotdates
-from matplotlib.dates import MONDAY
-from matplotlib.dates import MonthLocator, WeekdayLocator, DateFormatter
 
 
-import taiga_stats
-
-
-################# Constants #####################
 DOT_HEADER_FMT = """digraph {:s} {{
   labelloc="t";
   //labelfontsize="40"
@@ -31,9 +16,6 @@ DOT_HEADER_FMT = """digraph {:s} {{
   //orientation=landscape
 """
 
-
-
-################# Helper functions #####################
 
 def get_tag_str(tag):
     return "" if tag == taiga_stats.TAG_MATCH_ALL else tag
@@ -55,6 +37,7 @@ def get_stories_with_tag(project, tag):
         sys.exit(1)
     return ret_uss
 
+
 def get_us_stauts_id_from_name(project, name):
     statuses = project.list_user_story_statuses()
     for status in statuses:
@@ -62,12 +45,14 @@ def get_us_stauts_id_from_name(project, name):
             return status.id
     return None
 
+
 def get_us_status_name_from_id(project, status_id):
     statuses = project.list_user_story_statuses()
     for status in statuses:
         if status.id == status_id:
             return status.name
     return None
+
 
 def remove_closed_stories(project, uss):
     ret_uss = []
@@ -81,12 +66,15 @@ def get_statuses_sorted_by_order(project):
     statuses = project.list_user_story_statuses()
     return sorted(statuses, key=lambda status: status.order)
 
+
 def get_statuses_sorted_by_id(project):
     statuses = project.list_user_story_statuses()
     return sorted(statuses, key=lambda status: status.id)
 
+
 def get_status_id_sorted(project):
     return [status.id for status in get_statuses_sorted_by_order(project)]
+
 
 def get_status_and_names_sorted(project):
     status_ids = get_status_id_sorted(project)[::-1]
@@ -98,11 +86,12 @@ def get_status_and_names_sorted(project):
 
 
 def get_dot_header(name, title):
-    return taiga_stats.DOT_HEADER_FMT.format(name, title)
+    return DOT_HEADER_FMT.format(name, title)
 
 
 def get_dot_footer():
     return "}"
+
 
 def read_daily_cfd(path, tag):
     data_file = taiga_stats.CFD_DATA_FILE_FMT.format(get_tag_str(tag))
@@ -115,13 +104,13 @@ def read_daily_cfd(path, tag):
                 line = line.rstrip()
                 parts = line.split('\t')
                 if row == 0:
-                    data  = [[] for _ in range(len(parts) + 1)]
+                    data = [[] for _ in range(len(parts) + 1)]
                 else:
                     for col in range(len(parts)):
                         value = parts[col]
-                        if col == 0: # First col is dates
+                        if col == 0:  # First col is dates
                             value = dt.datetime.strptime(value, "%Y-%m-%d")
-                        elif col == 1: # Second col is annotations
+                        elif col == 1:  # Second col is annotations
                             pass
                         else:
                             value = int(value)
@@ -145,6 +134,7 @@ class assert_args(object):
 
     def __call__(self, func):
         dec = self
+
         def wrapper(args):
             for arg in dec.needed_args:
                 if arg not in args or args[arg] is None:
