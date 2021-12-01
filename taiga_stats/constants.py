994012821004b65321591ddc2689b11e5050e0af
Erik Westrup <erik.westrup@gmail.com>
2021-12-01 14:11:32 +0100

    Fix bug; XDG_CONFIG_HOME need to be set

--- taiga_stats/constants.py
+++ taiga_stats/constants.py
@@ -3,7 +3,7 @@ import os
 TAG_MATCH_ALL = "*"
 CUST_ATTRIB_DEPENDSON_NAME = "Depends On"
 
-CONF_FILE_PATH_XDG = os.environ["XDG_CONFIG_HOME"] + "/taiga-stats/taiga-stats.conf"
+CONF_FILE_PATH_XDG = os.getenv("XDG_CONFIG_HOME", os.environ["HOME"] + "/.config") + "/taiga-stats/taiga-stats.conf"
 CONF_FILE_PATH = "~/.taiga-stats.conf"
 CONF_FILE_NAME_FMT = "taiga-stats.conf.template"
 
