016587f9526b0fc851c0e780656d3ad06a95b25c
Erik Westrup <erik.westrup@gmail.com>
2021-12-01 14:29:44 +0100

    Fix importlib.metadata for python 3.7

--- taiga_stats/__init__.py
+++ taiga_stats/__init__.py
@@ -1,5 +1,8 @@
-import importlib.metadata
+try:
+    from importlib import metadata
+except ImportError:  # for Python<3.8. Reference: https://stackoverflow.com/a/59734959/265508
+    import importlib_metadata as metadata
 
 # Read version from pyproject.toml. Might not be correct in dev enviorment, but will be when distributed.
 # Reference: https://stackoverflow.com/a/67097076/265508
-__version__ = importlib.metadata.version("taiga-stats")
+__version__ = metadata.version("taiga-stats")
