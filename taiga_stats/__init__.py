6b784b0f48d1713173f07cec54f7b87177ad31bc
Erik Westrup <erik.westrup@gmail.com>
2021-12-17 10:53:51 +0100

    More reliable single definition version with single-source module

--- taiga_stats/__init__.py
+++ taiga_stats/__init__.py
@@ -1,8 +1,5 @@
-try:
-    from importlib import metadata
-except ImportError:  # for Python<3.8. Reference: https://stackoverflow.com/a/59734959/265508
-    import importlib_metadata as metadata
+from pathlib import Path
 
-# Read version from pyproject.toml. Might not be correct in dev enviorment, but will be when distributed.
-# Reference: https://stackoverflow.com/a/67097076/265508
-__version__ = metadata.version("taiga-stats")
+from single_source import get_version
+
+__version__ = get_version(__name__, Path(__file__).parent.parent)
